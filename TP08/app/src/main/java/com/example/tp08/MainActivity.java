d82088ab3eaa294b1eac8a4ed0b76d1a8a30f179
MBelenPiva <83927735+MBelenPiva@users.noreply.github.com>
2021-09-27 08:34:32 -0300

    menu

--- TP08/app/src/main/java/com/example/tp08/MainActivity.java
+++ TP08/app/src/main/java/com/example/tp08/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tp08;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -7,7 +8,10 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public SharedPreferences sharedPreferences;
@@ -25,12 +29,12 @@ public class MainActivity extends AppCompatActivity {
 
         ObtenerReferencias();
 
-        ReemplazarFragments();
+        ReemplazarFragments(R.id.main,listadoFragment, false);
 
     }
 
     private void ObtenerReferencias(){
-        main = (FrameLayout)findViewById(R.id.);
+        main = (FrameLayout)findViewById(R.id.main);
     }
 
     private void CrearFragments() {
@@ -57,5 +61,44 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public  boolean onCreateOptionsMenu(Menu menu)  {
+        //  Infla  el  menu:
+        //    Agrega  los  items  a  la  action  bar,  si  se  encuentra presente.
+        getMenuInflater().inflate(R.menu.menu,  menu);
 
+        //  You  must  return  true  for  the  menu  to  be  displayed;
+        //  if  you  return  false  it  will  not  be  shown.
+        return  true; }
+
+
+    @Override
+    public  boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        boolean blnReturn = true;
+        String strTitle;
+
+        strTitle = item.getTitle().toString(); //  Obtengo el  Titulo  de  MenuItem  presionado.
+        switch (item.getItemId()) {
+            case R.id.action_IrAListado:
+                Toast.makeText(getApplicationContext(), strTitle, Toast.LENGTH_LONG).show();
+                ReemplazarFragments(R.id.main,listadoFragment,false);
+                break;
+            case R.id.action_IrAInsertar:
+                Toast.makeText(getApplicationContext(), strTitle, Toast.LENGTH_LONG).show();
+                ReemplazarFragments(R.id.main,insertarFragment,false);
+                break;
+            default:
+                blnReturn = false;
+                break;
+        }
+        return blnReturn;
+    }
+
+
+    @Override
+    public  boolean onPrepareOptionsMenu(Menu  menu)  {
+        MenuItem  item  =  menu.findItem(R.id.action_IrAListado);
+        item.setEnabled(false);
+        return  super.onPrepareOptionsMenu(menu);
+    }
 }
