b7f609546ad176c121bdd7ffe2a697c2d681799f
MBelenPiva <83927735+MBelenPiva@users.noreply.github.com>
2021-10-05 08:53:51 -0300

    cambios api

--- TP08/app/src/main/java/com/example/tp08/LoginActivity.java
+++ TP08/app/src/main/java/com/example/tp08/LoginActivity.java
@@ -2,12 +2,24 @@ package com.example.tp08;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static java.security.AccessController.getContext;
 
 public class LoginActivity extends AppCompatActivity {
     public SharedPreferences sharedPreferences;
@@ -59,4 +71,75 @@ public class LoginActivity extends AppCompatActivity {
 
         }
     };
-}
+    private class tareaAsincronicaMovies extends  AsyncTask<Void,Void,String> {
+
+        private ProgressDialog dialog;
+        private Context context;
+        private String strURL = "http://api.polshu.com.ar/api/v1/login/usuario/password";
+
+        public tareaAsincronicaMovies() {
+            this.context = getContext();
+            dialog = new ProgressDialog(context);
+        }
+
+        /*public void setURl(String strUrl) {
+            strURL = strUrl;
+        }*/
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            this.dialog.setMessage("Buscando la información...");
+            this.dialog.show();
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            HttpURLConnection conexion = null;
+            URL strAPIUrl;
+            String strResultado = "";
+            try {
+                conexion = (HttpURLConnection) strURL.openConnection();
+                conexion.setRequestMethod("GET");
+                int intStatusCode = conexion.getResponseCode();
+                if (intStatusCode == 200) {
+                    strResultado = StreamHelper.GetFullStringFromInputReader(conexion.getInputStream());
+                } else {
+                    //OCURRIO ALGUN ERROR
+                }
+            } catch (MalformedURLException e) {
+                Log.d("E", e.toString());
+                e.printStackTrace();
+            } catch (IOException e) {
+                Log.d("E", e.toString());
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                Log.d("E", e.toString());
+                e.printStackTrace();
+            } catch (Exception e) {
+                Log.d("E", e.toString());
+                //Toast.makeText(getContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+            } finally {
+                if (conexion != null) {
+                    conexion.disconnect();
+                }
+            }
+            return strResultado;
+        }
+
+        @Override
+        protected void onPostExecute(String resultado) {
+            super.onPostExecute(resultado);
+            ArrayList<Pelicula> listaPeliculas;
+            listaPeliculas = ParseElResultado(strURL);
+            PeliculaArrayAdapter adapter;
+            adapter = new PeliculaArrayAdapter(getContext(), R.layout.fragment_movie, listaPeliculas);
+            lvDatos.setAdapter(adapter);
+            if (dialog.isShowing()) {
+                dialog.dismiss();
+            }
+            //Toast.makeText(context, resultado, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    }
---
+++ TP08/app/src/main/java/com/example/tp08/Utilis/LogHelper.java
@@ -0,0 +1,16 @@
+package com.example.tp08.Utilis;
+
+import android.util.Log;
+
+public class LogHelper {
+
+    public static final String LOG_TAG = "";
+
+    public static void d(String strMensaje){
+        Log.d(LOG_TAG, strMensaje);
+    }
+
+    public static void enArchivo(String message){
+
+    }
+}
